253363a3391d2eaf1d5a1d9cc2cee67c7fe5f0d1
Rishabh-Truwix <rishabhr@truwix.com>
2025-02-18 15:18:58 +0530

    homepage done

--- src/components/AboutpageComponents/Section2.jsx
+++ src/components/AboutpageComponents/Section2.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Section2() {
     return (
         <div className='w-full h-auto flex flex-col px-5 md:px-10 lg:px-20'>
-            <h1 className='text-[#374836]'>|  Our Mission  |</h1>
+            <h1 className='text-[#374836] text-center text-lg'>|  Our Mission  |</h1>
         </div>
     )
 }
