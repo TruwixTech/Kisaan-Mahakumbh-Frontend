992f977f11980b6577eea3ebae0b3a79df6f1581
Aftab-trwuix <aftaba@truwix.com>
2025-02-18 18:31:05 +0530

    latest updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ function Header() {
             <NavLink to="/news" className="text-[#01210F] text-xl font-semibold" onClick={() => setIsOpen(false)}>News</NavLink>
             <NavLink to="/contact" className="text-[#01210F] text-xl font-semibold" onClick={() => setIsOpen(false)}>Contact</NavLink>
             <button className='px-3 py-2 bg-[#C39337] rounded-xl text-white font-semibold' onClick={() => setIsOpen(false)}>Join Now</button>
-            <button className='px-5 py-2 bg-[#01210F] rounded-xl text-white font-semibold' onClick={() => setIsOpen(false)}>Register for Event</button>
+            <NavLink to="/registration" className='px-5 py-2 bg-[#01210F] rounded-xl text-white font-semibold cursor-pointer' >Register for Event</NavLink>
           </div>
         )}
 
@@ -43,7 +43,7 @@ function Header() {
         </div>
         <div className='w-auto h-auto hidden items-center gap-6 lg:flex xl:gap-8'>
           <button className='px-3 py-2 bg-[#C39337] rounded-xl text-white font-semibold xl:py-2.5'>Join Now</button>
-          <button className='px-5 py-2 bg-[#01210F] rounded-xl text-white font-semibold xl:py-2.5'>Register for Event</button>
+          <NavLink to="/registration" className='px-5 py-2 bg-[#01210F] rounded-xl text-white font-semibold xl:py-2.5'>Register for Event</NavLink>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import UserRegistrationForm from './components/UserRegistrationForm.jsx'
+import ContactForm from './pages/Contact.jsx'
 
 
 const router = createBrowserRouter(
@@ -11,6 +13,9 @@ const router = createBrowserRouter(
     <>
       <Route path="/" element={<App />} >
         <Route path="/" element={<Home />} />
+        <Route path='/registration' element={<UserRegistrationForm />} />
+        <Route path='/contact' element={<ContactForm />} />
+
       </Route>
     </>
   )
