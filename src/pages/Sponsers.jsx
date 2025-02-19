d00830999475b2165d161db1ef7fa3ad755d5f75
YashTyagi09876 <yashtyagi3357@gmail.com>
2025-02-19 15:31:15 +0530

    work on sponser page

--- src/pages/Sponsers.jsx
+++ src/pages/Sponsers.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Section1 from '../components/SponserspageComponents/Section1'
+import Section2 from '../components/SponserspageComponents/Section2'
+
 
 function Sponsers() {
   return (
     <div className='w-full h-auto flex flex-col'>
         <Section1 />
+        <Section2 />
+        
     </div>
   )
 }
