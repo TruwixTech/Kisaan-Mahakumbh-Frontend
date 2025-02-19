f6deb10ab09acb9da9489b68ac04ea418160ab6f
Aftab-trwuix <aftaba@truwix.com>
2025-02-19 15:38:44 +0530

    new changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Section1 from '../components/AboutpageComponents/Section1'
-import Section2 from '../components/AboutpageComponents/Section2'
+import React from "react";
+import Section1 from "../components/AboutpageComponents/Section1";
+import Section2 from "../components/AboutpageComponents/Section2";
+import Partners from "../components/HomePageComponents/Partners";
 
 function About() {
-    return (
-        <div className='w-full h-auto flex flex-col'>
-            <Section1 />
-            <Section2 />
-        </div>
-    )
+  return (
+    <div className="w-full h-auto flex flex-col">
+      <Section1 />
+      <Section2 />
+      <Partners />
+    </div>
+  );
 }
 
-export default About
+export default About;
