ba2d32ab8cb3d0a3789b4798aadf7c07b72e3624
TruwixTech <truwixtech@gmail.com>
2025-02-24 11:11:48 +0530

    Revert "Dev"

--- src/components/HomePageComponents/Section5.jsx
+++ src/components/HomePageComponents/Section5.jsx
@@ -1,62 +1,34 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import kisaan from "../../assets/kisaan.png";
 import icon1 from "../../assets/icon1.svg";
 import icon2 from "../../assets/icon2.svg";
 import icon3 from "../../assets/icon3.svg";
 import icon4 from "../../assets/icon4.svg";
-import Flipped from '../../assets/Flipped.jpg';
 
 const AgritechHighlight = () => {
-  const [isFlipped, setIsFlipped] = useState(false);
-  const sectionRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setIsFlipped(true);
-          }
-        });
-      },
-      { threshold: 0.5 } // Triggers when 50% of the section is visible
-    );
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
-    }
-
-    return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
-      }
-    };
-  }, []);
-
   return (
     <div className="w-full bg-[#e7e7e7]  flex flex-col gap-6 lg:flex-row items-center justify-center py-12 px-5 lg:px-20 font-roboto text-[#374836] font-[Roboto]">
-      <div ref={sectionRef} className="w-full lg:w-1/2 h-full flex justify-center">
-        <div className="flip-container">
-          <div className={`flip-card ${isFlipped ? "flipped" : ""}`}>
-            {/* Front Image */}
-            <div className="flip-card-front"></div>
-            {/* Back Image */}
-            <div className="flip-card-back"></div>
-          </div>
-        </div>
+      <div className="w-full lg:w-1/2 h-full">
+        {/* Placeholder for Image */}
+        <div className=" w-full h-full rounded-lg"></div>
+        <img src={kisaan} alt="kisan image" className="mx-auto rounded-3xl" style={{
+          boxShadow: '6px 7px 12.9px 3px rgba(0, 0, 0, 0.25)', // Custom box shadow
+        }} />
+        {/* Content Section */}
       </div>
-
-      {/* Content Section */}
       <div className="w-full lg:w-1/2">
         <p className="text-gray-500 text-xl font-bold text-center md:text-left">
-          | Event Highlight |
+          | Highlight |
         </p>
         <h2 className="text-3xl md:text-5xl lg:text-4xl xl:text-4xl font-bold text-[#374836] mt-2 leading-tight text-center md:text-left">
           PIONEERING THE <br /> FUTURE OF{" "}
           <span className="text-[#146a3a]">AGRITECH</span>
         </h2>
         <p className="text-[#374836] mt-3 text-center md:text-left">
-          Kisan Kumbh 2025 aims to bridge this gap by bringing together all stakeholders—including small and large farmers, model farmers, consumers, financial institutions, NGOs, policymakers, and agriculture experts—on a single platform.{" "}
+          vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+          sit aspernatur aut odit aut fugit, sed quia consequuntur numquam eius
+          modi tempora incidunt ut labore et dolore magnam aliquam consequuntur
+          magni dolores eos qui ratione v{" "}
         </p>
 
         {/* Feature Grid */}
@@ -65,12 +37,12 @@ const AgritechHighlight = () => {
             <div className=" flex gap-4 items-center">
               <img src={icon1} alt="" />
               <h3 className="text-xl lg:text-xl xl:text-xl font-semibold text-[#374836]">
-                AgriTech & Innovation Showcase
+                Enhanced Productivity
               </h3>
             </div>
             <div>
               <p className="text-[#7F9080] text-base lg:text-base xl:text-base">
-                Discover cutting-edge drones, AI-powered precision farming, smart irrigation systems, and climate-resilient technologies revolutionizing agriculture.
+                Implement advanced farming techniques that significantly boost crop yields and overall farm productivity
               </p>
             </div>
           </div>
@@ -78,12 +50,12 @@ const AgritechHighlight = () => {
             <div className="flex gap-4 items-center">
               <img src={icon2} alt="" />
               <h3 className="text-xl lg:text-xl xl:text-xl font-semibold text-[#374836]">
-                Expert Talks & Panel Discussions
+                Cost Savings
               </h3>
             </div>
             <div>
               <p className="text-[#7F9080] text-base">
-                Engage with leading scientists, policymakers, successful farmers, and AgriTech entrepreneurs as they discuss the future of agriculture.
+                Lower your operational costs through efficient resource management and reduced dependency on expensive
               </p>
             </div>
           </div>
@@ -91,12 +63,12 @@ const AgritechHighlight = () => {
             <div className="flex gap-4 items-center">
               <img src={icon3} alt="" />
               <h3 className="text-xl lg:text-xl xl:text-xl font-semibold text-[#374836]">
-                Government Schemes & Funding Support
+                Sustainable Practices
               </h3>
             </div>
             <div>
               <p className="text-[#7F9080] text-base">
-                Understand key initiatives like Mission Mausam, Didi Drone Yojana, Prakritik Kheti Mission, and AgriTech 4.0.
+                Adopt eco-friendly methods that promote soil health, reduce water usage, and minimize chemical inputs, ensuring long-term
               </p>
             </div>
           </div>
@@ -104,12 +76,12 @@ const AgritechHighlight = () => {
             <div className="flex gap-4 items-center">
               <img src={icon4} alt="" />
               <h3 className="text-xl lg:text-xl xl:text-xl font-semibold text-[#374836]">
-                Startup & Farmer Excellence Awards
+                Expert Guidance
               </h3>
             </div>
             <div>
               <p className="text-[#7F9080] text-base">
-                Recognizing innovative Agri startups, Farmer Producer Organizations (FPOs), and progressive farmers.
+                Receive personalized support and advice from our team of agricultural experts to optimize your farming operations
               </p>
             </div>
           </div>
--- src/components/SponserspageComponents/Section1.jsx
+++ src/components/SponserspageComponents/Section1.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../Header'
+import Hero from '../../assets/Hero.jpg'
+import { FaHome } from "react-icons/fa";
+
 import Sponser1 from '../../assets/sponser1.png'
 import Sponser2 from '../../assets/sponser2.png'
 import Sponser3 from '../../assets/sponser3.png'
 import Sponser4 from '../../assets/sponser4.png'
+
 
 function Section1() {
     const heroesData = [
@@ -60,7 +65,33 @@ function Section1() {
 
     return (
         <div className='w-full h-auto flex flex-col bg-[#e7e7e7]'>
+            <div className='relative z-40' style={{
+                backgroundImage: `url(${Hero})`,
+                backgroundSize: 'cover',  // Adjust size as per requirement
+                backgroundPosition: 'top',  // Ensures the gradient starts from the left
+                backgroundRepeat: 'no-repeat',
+            }}>
+                <div
+                    className="w-full h-full flex flex-col absolute z-10"
+                    style={{
+                        backgroundImage: `linear-gradient(90deg, rgba(0, 0, 0, 0.7) 0%, rgba(19, 19, 19, 0.49) 49%, rgba(131, 131, 131, 0) 100%)`,
+                        backgroundSize: '100% 100%',  // Adjust size as per requirement
+                        backgroundPosition: 'left center',  // Ensures the gradient starts from the left
+                        backgroundRepeat: 'no-repeat',
+                    }}
+                >
+                </div>
+                <Header />
+            </div>
             <div className='w-full h-auto flex flex-col px-5 md:px-10 lg:px-20 py-10 bg-[#e7e7e7] md:gap-2'>
+                <h1 className='text-center text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl mb-6 font-[Roboto]'>Sponsors</h1>
+                <div className='w-full h-auto flex items-center justify-center font-[Segoe UI]'>
+                    <span className='flex items-center justify-center text-black gap-1'>
+                        <FaHome size={20} />
+                        Home
+                    </span>
+                    <span className='text-[#0000004D]'> - Pages - Sponsors</span>
+                </div>
                 <div className="bg-[#e7e7e7] py-10 font-[Roboto]">
                     {/* Header Section */}
                     <div className="text-center">
