edf102ec8791c22c7677b6329131195db8d8b22b
tarun-truwix <taruns@truwix.com>
2025-02-25 20:18:44 +0530

    home page changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import Footer from './components/Footer'
-import { Outlet } from 'react-router-dom'
-import Donation from './components/Donation'
-import RegisterPopup from './components/RegisterPopup'
-import RegisterNow from './components/RegisterNow'
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import Donation from "./components/Donation";
+import RegisterPopup from "./components/RegisterPopup";
+import RegisterNow from "./components/RegisterNow";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-
   return (
     <>
-      <Outlet />
-      <Donation />
-      <RegisterNow />
-      <RegisterPopup />
-      <Footer />
+      <div className="w-auto h-auto">
+        <Outlet />
+        <ScrollToTop />
+        <Donation />
+        <RegisterNow />
+        <RegisterPopup />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
